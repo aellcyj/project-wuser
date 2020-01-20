6b025d3c02f14d567682c4479f071062c9d46ff4
aellcyj <aellcyj@gmail.com>
2020-01-19 21:50:19 -0400

    projeto criado

---
+++ src/br/com/java/program/PrimeiroProgram.java
@@ -0,0 +1,14 @@
+package br.com.java.program;
+
+public class PrimeiroProgram {
+	public static void main(String args[]) {
+		
+		System.out.println("elcy arruda");
+		
+		System.out.println("primeiro teste concluido");
+		
+		System.out.println("agora vai");
+		
+	}
+
+}
